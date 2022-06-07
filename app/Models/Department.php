3bc61a1566041c151214fb6d9bbae59844ce0931
DoNgocSon <sonnvc2001@gmail.com>
2022-06-07 17:15:13 +0700

    them Model Department

---
+++ app/Models/Department.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Department extends Model
+{
+    protected $fillable = [
+        'department_ID,
+        department_name,
+        department_desc,
+        department_imgURL,
+        create_at,
+        update_at'
+    ];
+}
