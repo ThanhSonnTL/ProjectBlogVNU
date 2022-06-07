b9a48eca8f7b60f363fc802c23a0f40b10ab5f1a
DoNgocSon <sonnvc2001@gmail.com>
2022-06-07 17:21:39 +0700

    git commit -m 'tao cac migration'

---
+++ database/migrations/2022_06_07_093802_create_lectures_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lectures', function (Blueprint $table) {
+            $table->increments('lecturer_ID');
+            $table->string('lecturer_name');
+            $table->string('lecturer_gender');
+            $table->string('lecturer_email');
+
+            $table->unsignedInteger('department_ID');
+            $table->foreign('department_ID')->references('department_ID')->on('departments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lectures');
+    }
+};
