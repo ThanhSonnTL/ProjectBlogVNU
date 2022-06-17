83874fef316b4dda989ec339d109f5a224f16625
DoNgocSon <sonnvc2001@gmail.com>
2022-06-17 08:30:45 +0700

    them Controller xu ly login

--- app/Http/Controllers/LoginControllerResoucre.php
+++ app/Http/Controllers/LoginControllerResoucre.php
@@ -2,13 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginControllerResoucre extends Controller
 {
     public function auth_login()
     {
-        //Get all blogs
+        return view('auth.login');
+    }
+    public function getFormLogin()
+    {
         return view('admin.home');
+    }
+    public function submitLogin(Request $request)
+    {
+      
+        $messages = [
+            'email.required' => 'Email chưa có thông tin',
+            'email.email' => 'Email không hợp lệ vui lòng kiểm tra lại',
+            'email.exists' => 'Email chưa có trong hệ thống',
+            'password.required' => 'Password chưa có thông tin'
+        ];
+        $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required',
+        ],  $messages );
+
+        $email = $request->email;
+        $password = $request->password;
+        if (Auth::attempt([
+            'email' => $email,
+            'password' => $password,
+        ])) {
+            $user = User::where('email', $email)->first();
+            Auth::login($user);
+            $username=Auth::user()->name;
+            return view('admin.home',['username' => $username]);
+        } else {
+            return redirect()->route('formLogin')->with('message', 'Tài khoản hoặc mật khẩu chưa chính xác!');
+        }
+    }
+
+    public function submitLogoutForm(){
+        Auth::logout();
+        return redirect()->route('formLogin');
     }
 }
